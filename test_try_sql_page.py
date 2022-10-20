c7742156ebdc42b0dba374d619b8df1d766d1cc7
Никита Кучин <varlok009@mail.ru>
2022-10-20 12:00:40 +0600

    add readme, add hint

--- test_try_sql_page.py
+++ test_try_sql_page.py
@@ -10,20 +10,20 @@ class TestSqlPage:
         self.page.open()
         yield
 
-    def test_request_select_all(self):
+    def test_request_select_all(self) -> None:
         test_query = "SELECT * FROM Customers;"
         self.page.send_query(test_query)
 
         self.page.should_be_result_table()
         self.page.checking_column_values_in_a_row({'CustomerName': 'Split Rail Beer & Ale', 'City': 'Lander'})
 
-    def test_request_with_query(self):
+    def test_request_with_query(self) -> None:
         test_query = "SELECT * FROM Customers WHERE City='London';"
         self.page.send_query(test_query)
 
         assert len(self.page.get_all_table_rows()) == 6, 'Number costumers where city = London should be 6'
 
-    def test_create_a_new_entry(self):
+    def test_create_a_new_entry(self) -> None:
         test_query = "INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country" \
                      ") VALUES ('Nikita', 'Varlok', 'Mangilik', 'Astana', '123456', 'Kazakhstan');"
         self.page.send_query(test_query)
