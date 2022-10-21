b0b050fb57cbfaca1de361c1b3003a76c7cd7d32
Никита Кучин <varlok009@mail.ru>
2022-10-21 22:12:45 +0600

    add fourth test and refactor

--- test_try_sql_page.py
+++ test_try_sql_page.py
@@ -11,25 +11,62 @@ class TestSqlPage:
         yield
 
     def test_request_select_all(self) -> None:
+        test_data = {
+            'CustomerName': 'Split Rail Beer & Ale',
+            'City': 'Lander'
+        }
         test_query = "SELECT * FROM Customers;"
         self.page.send_query(test_query)
 
         self.page.should_be_result_table()
-        self.page.checking_column_values_in_a_row({'CustomerName': 'Split Rail Beer & Ale', 'City': 'Lander'})
+        self.page.checking_column_values_in_a_row(test_data)
 
     def test_request_with_query(self) -> None:
-        test_query = "SELECT * FROM Customers WHERE City='London';"
+        test_data = {
+            'City': 'London',
+        }
+        test_query = f"SELECT * FROM Customers WHERE City='{test_data['City']}';"
         self.page.send_query(test_query)
 
-        assert len(self.page.get_all_table_rows()) == 6, 'Number costumers where city = London should be 6'
+        assert len(self.page.get_all_table_rows()) == 6, f"Number costumers where " \
+                                                         f"City = {test_data['City']} should be 6"
 
-    def test_create_a_new_entry(self) -> None:
-        test_query = "INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country" \
-                     ") VALUES ('Nikita', 'Varlok', 'Mangilik', 'Astana', '123456', 'Kazakhstan');"
+    def test_create_a_new_row(self) -> None:
+        test_data = {
+            # 'CustomerID': '2',
+            'CustomerName': 'Nikita',
+            'ContactName': 'Varlok',
+            'Address': 'Mangilik',
+            'City': 'Astana',
+            'PostalCode': '123456',
+            'Country': 'Kazakhstan'
+        }
+        quote = "'"
+        test_query = f"INSERT INTO Customers ({', '.join(f'{column}' for column in test_data.keys())}) VALUES " \
+                     f"({', '.join(f'{quote}{value}{quote}' for value in test_data.values())});"
         self.page.send_query(test_query)
         self.page.check_request_confirmation_message()
-
-        test_query = "SELECT * FROM Customers WHERE CustomerName='Nikita'"
+        test_query = f"SELECT * FROM Customers WHERE CustomerName='{test_data['CustomerName']}'"
 
         self.page.send_query(test_query)
-        self.page.checking_column_values_in_a_row({'CustomerName': 'Nikita', 'ContactName': 'Varlok', 'City': 'Astana'})
+        self.page.checking_column_values_in_a_row(test_data)
+
+    def test_update_row(self) -> None:
+        test_data = {
+            # 'CustomerID': '2',
+            'CustomerName': 'Nikita',
+            'ContactName': 'Varlok',
+            'Address': 'Mangilik',
+            'City': 'Astana',
+            'PostalCode': '123456',
+            'Country': 'Kazakhstan'
+        }
+        quote = "'"
+        test_query = f"UPDATE Customers SET " \
+                     f"{', '.join(f'{item[0]}={quote}{item[1]}{quote}' for item in test_data.items())} " \
+                     f"WHERE CustomerID=2;"
+        self.page.send_query(test_query)
+        self.page.check_request_confirmation_message()
+        test_query = "SELECT * FROM Customers WHERE CustomerID=2;"
+        self.page.send_query(test_query)
+        self.page.checking_column_values_in_a_row(test_data)
